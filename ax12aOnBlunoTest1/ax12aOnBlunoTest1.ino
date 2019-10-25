8c46cc1276968a162df73c86dd975944e130412c
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-10-25 19:47:48 -0400

    add 1st (and only) test so far

---
+++ ax12aOnBlunoTest1/ax12aOnBlunoTest1.ino
@@ -0,0 +1,12 @@
+#include <ax12.h>
+#define port 1
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  SetPosition(1,512);
+  SetPosition(7,400);
+  SetPosition(13,400);
+}
