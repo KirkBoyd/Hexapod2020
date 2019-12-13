91756ca93c68296409563236bddeb29fa9e5cc6f
rwbot <rwbotx@gmail.com>
2019-12-13 15:33:53 -0500

    fix: comment old link generator in tibia

--- config/aliases.sh
+++ config/aliases.sh
@@ -5,6 +5,7 @@
 #       echo "source ~/hex_ws/src/hexapod/config/aliases.sh" >> ~/.bashrc
 
 
+
 alias srcb='source ~/.bashrc'
 alias sws='source devel/setup.bash'
 alias swsx='source ~/hex_ws/devel/setup.bash'
@@ -14,5 +15,6 @@ alias kz='killall gazebo & killall gzserver & killall gzclient'
 alias rlh="swsx; roslaunch hexapod hexapod.launch"
 alias cdx='cd ~/hex_ws/src/hexapod/'
 
-alias gitconRW="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
-alias gitconKIRK="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'Kirk Boyd'; git config --global user.email 'kirkland.boyd@trincoll.edu'"
+git config --global credential.helper 'cache --timeout=999999'
+alias gitconRW="git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
+alias gitconKIRK="git config --global user.name 'Kirk Boyd'; git config --global user.email 'kirkland.boyd@trincoll.edu'"
